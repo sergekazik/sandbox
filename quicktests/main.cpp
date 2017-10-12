8651697e8ce6ba4b93e356c40da1144e461b7b9c
sergei <sergei.kazantsev@ring.com>
2017-10-11 19:38:10 -0700

    automatic save Wed Oct 11 19:38:10 PDT 2017

--- quicktests/main.cpp
+++ quicktests/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
+    const char * hello = "hello";
+    printf("%-10s %s\n", hello, hello);
     return 0;
 }
 
