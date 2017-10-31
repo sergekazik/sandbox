6510b6ebd63d557690c99cbc6d54c9416e479e14
sergei <sergei.kazantsev@ring.com>
2017-10-31 13:27:20 -0700

    automatic save Tue Oct 31 13:27:20 PDT 2017

--- quicktests/main.cpp
+++ quicktests/main.cpp
@@ -9,6 +9,12 @@ int main()
     cout << "Hello World!" << endl;
     const char * hello = "hello";
     printf("%-10s %s\n", hello, hello);
+
+    for (int i = 5; i < 15; i++)
+    {
+        printf("%-2d) %-40s*\n", i, "hell");
+    }
+
     return 0;
 }
 
